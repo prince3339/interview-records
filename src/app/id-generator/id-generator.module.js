4c21d9941b2366081db2318b5ee9cd212ea283ce
Prince <prince3339@ymail.com>
2017-08-16 18:25:32 +0600

    updated

--- src/app/id-generator/id-generator.module.js
+++
@@ -1,9 +0,0 @@
-var angular = require('angular');
-(function(angular) {
-    'use strict';
-    angular
-        .module('idGenerator', [])
-        .run(function() {
-            console.log('ID generator App loaded');
-        });
-})(angular);
--- src/app/interview-record/service/index.js
+++
@@ -1,1 +0,0 @@
-require('./interview-record.service');
